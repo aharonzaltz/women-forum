1ec1d46ac7e529a43778d47c5f4e285c3908a3bb
aharon_anopa <aharon@anopa.co.il>
2024-02-18 15:47:44 +0200

    second commit

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+// phpBB 3.3.x auto-generated configuration file
+// Do not change anything in this file!
+$dbms = 'phpbb\\db\\driver\\mysqli';
+$dbhost = 'db-mysql-fra1-20941-do-user-15864181-0.c.db.ondigitalocean.com';
+$dbport = '25060';
+$dbname = 'defaultdb';
+$dbuser = 'doadmin';
+$dbpasswd = 'AVNS_YJ62rskUCxbtVlPS44X';
+$table_prefix = 'phpbb_';
+$phpbb_adm_relative_path = 'adm/';
+$acm_type = 'phpbb\\cache\\driver\\file';
+
+@define('PHPBB_INSTALLED', true);
+@define('PHPBB_ENVIRONMENT', 'production');
+// @define('DEBUG_CONTAINER', true);
